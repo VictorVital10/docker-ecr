c87a220708ed067d3d5bc4f3647648b06660a866
VictorVital10 <vitassa2003@gmail.com>
2026-02-24 20:42:27 -0300

    Creating SG and rules

--- fase-02/ec2.tf
+++ fase-02/ec2.tf
@@ -4,6 +4,7 @@ resource "aws_instance" "vav-instance" {
   ami = "ami-08dd439af9c3f1639"
   instance_type = "t3.micro"
   key_name = "vav-kp"
+  vpc_security_group_ids = [aws_security_group.vav-sg.id]
 
   tags = {
     Name = "VAV-Instance"
@@ -13,7 +14,7 @@ resource "aws_instance" "vav-instance" {
 
 # Key Pair SSH.
 
-resource "aws_key_pair" "vav-kp" {
+resource "aws_key_pair" "vav-kp-" {
     key_name = "vav-kp"
     public_key = file("C:/Users/Vitor Vital/.SSH/VAV-Instance/PUBK")
 }
@@ -28,4 +29,39 @@ resource "aws_security_group" "vav-sg" {
   tags = {
     Provisioned = "Terraform"
   }
+}
+
+# Ingress Rule
+
+resource "aws_vpc_security_group_ingress_rule" "allow-ssh" {
+  security_group_id = aws_security_group.vav-sg.id
+  cidr_ipv4         = "192.168.0.3/32"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow-http" {
+  security_group_id = aws_security_group.vav-sg.id
+  cidr_ipv4         = "192.168.0.3/32"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow-https" {
+  security_group_id = aws_security_group.vav-sg.id
+  cidr_ipv4         = "192.168.0.3/32"
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+# Egress Rule
+
+resource "aws_vpc_security_group_egress_rule" "allow-all-outbound" {
+  security_group_id = aws_security_group.vav-sg.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  ip_protocol = -1
 }
